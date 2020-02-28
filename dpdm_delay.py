d79108bd674fa1d18dbcc9977e52a6440fd532f0
XueXiao-Physics <xuexiao@mail.itp.ac.cn>
2020-02-29 00:34:16 +0800

    .

--- dpdm_delay.py
+++ dpdm_delay.py
@@ -36,7 +36,7 @@ def Gamma(a, loc, scale, size=None):
 
 
 @parameter.function
-def delay_U1B(toas, pos, log10_ma, log10_eps, normA1_e, normA2_e, normA3_e, normA_p, phase1_e, phase2_e, phase3_e, phase_p):
+def delay_U1B(toas, pos, log10_ma, log10_eps, normA2_1_e, normA2_2_e, normA2_3_e, normA2_p, phase1_e, phase2_e, phase3_e, phase_p):
 
     e   = np.sqrt( sc.alpha * 4 * np.pi )
     rvt2s  = sc.hbar / ( sc.electron_volt )
@@ -50,10 +50,10 @@ def delay_U1B(toas, pos, log10_ma, log10_eps, normA1_e, normA2_e, normA3_e, norm
     q2m_e = bq2m
     q2m_p = bq2m
 
-    dx_e1 = - A0 * normA1_e * eps * e * (1.0 / ma) * q2m_e * np.sin(twopif * toas + phase1_e)
-    dx_e2 = - A0 * normA2_e * eps * e * (1.0 / ma) * q2m_e * np.sin(twopif * toas + phase2_e)
-    dx_e3 = - A0 * normA3_e * eps * e * (1.0 / ma) * q2m_e * np.sin(twopif * toas + phase3_e)    
-    dx_p = - A0 * normA_p * eps * e * (1.0 / ma) * q2m_p * np.sin(twopif * toas + phase_p)
+    dx_e1 = - A0 * np.sqrt(normA2_1_e) * eps * e * (1.0 / ma) * q2m_e * np.sin(twopif * toas + phase1_e)
+    dx_e2 = - A0 * np.sqrt(normA2_2_e) * eps * e * (1.0 / ma) * q2m_e * np.sin(twopif * toas + phase2_e)
+    dx_e3 = - A0 * np.sqrt(normA2_3_e) * eps * e * (1.0 / ma) * q2m_e * np.sin(twopif * toas + phase3_e)    
+    dx_p = - A0 * np.sqrt(normA2_p) * eps * e * (1.0 / ma) * q2m_p * np.sin(twopif * toas + phase_p)
 
     dx_e = pos[0]*dx_e1 + pos[1]*dx_e2 + pos[2]*dx_e3
 
@@ -61,7 +61,7 @@ def delay_U1B(toas, pos, log10_ma, log10_eps, normA1_e, normA2_e, normA3_e, norm
 
 
 @parameter.function
-def delay_U1BL(toas, pos, log10_ma, log10_eps, normA1_e, normA2_e, normA3_e, normA_p, phase1_e, phase2_e, phase3_e, phase_p):
+def delay_U1BL(toas, pos, log10_ma, log10_eps, normA2_1_e, normA2_2_e, normA2_3_e, normA2_p, phase1_e, phase2_e, phase3_e, phase_p):
 
     e   = np.sqrt( sc.alpha * 4 * np.pi )
     rvt2s  = sc.hbar / ( sc.electron_volt )
@@ -74,10 +74,10 @@ def delay_U1BL(toas, pos, log10_ma, log10_eps, normA1_e, normA2_e, normA3_e, nor
     q2m_e = bq2m / 2.
     q2m_p = bq2m
         
-    dx_e1 = - A0 * normA1_e * eps * e * (1.0 / ma) * q2m_e * np.sin(twopif * toas + phase1_e)
-    dx_e2 = - A0 * normA2_e * eps * e * (1.0 / ma) * q2m_e * np.sin(twopif * toas + phase2_e)
-    dx_e3 = - A0 * normA3_e * eps * e * (1.0 / ma) * q2m_e * np.sin(twopif * toas + phase3_e)    
-    dx_p = - A0 * normA_p * eps * e * (1.0 / ma) * q2m_p * np.sin(twopif * toas + phase_p)
+    dx_e1 = - A0 * np.sqrt(normA2_1_e) * eps * e * (1.0 / ma) * q2m_e * np.sin(twopif * toas + phase1_e)
+    dx_e2 = - A0 * np.sqrt(normA2_2_e) * eps * e * (1.0 / ma) * q2m_e * np.sin(twopif * toas + phase2_e)
+    dx_e3 = - A0 * np.sqrt(normA2_3_e) * eps * e * (1.0 / ma) * q2m_e * np.sin(twopif * toas + phase3_e)    
+    dx_p = - A0 * np.sqrt(normA2_p) * eps * e * (1.0 / ma) * q2m_p * np.sin(twopif * toas + phase_p)
 
     dx_e = pos[0]*dx_e1 + pos[1]*dx_e2 + pos[2]*dx_e3
 
@@ -85,8 +85,8 @@ def delay_U1BL(toas, pos, log10_ma, log10_eps, normA1_e, normA2_e, normA3_e, nor
 
 
 
-def dpdm_block(model=None,log10_ma=None,log10_eps=None,normA1_e=None,normA2_e=None,normA3_e=None,
-                normA_p=None,phase1_e=None,phase2_e=None,phase3_e=None,phase_p=None):
+def dpdm_block(model=None,log10_ma=None,log10_eps=None,normA2_1_e=None,normA2_2_e=None,normA2_3_e=None,
+                normA2_p=None,phase1_e=None,phase2_e=None,phase3_e=None,phase_p=None):
 
     name='x_dp'
 
@@ -95,11 +95,11 @@ def dpdm_block(model=None,log10_ma=None,log10_eps=None,normA1_e=None,normA2_e=No
 
     log10_eps = parameter.Uniform(-28.0, -16.0)(name + '_log10_eps')
 
-    normA1_e = Gamma(3.141, 0, 0.282)(name + "_normA1_e")
-    normA2_e = Gamma(3.141, 0, 0.282)(name + "_normA2_e")
-    normA3_e = Gamma(3.141, 0, 0.282)(name + "_normA3_e")
+    normA2_1_e = Gamma(1, 0, 1)(name + "_normA2_1_e")
+    normA2_2_e = Gamma(1, 0, 1)(name + "_normA2_2_e")
+    normA2_3_e = Gamma(1, 0, 1)(name + "_normA2_3_e")
 
-    normA_p = Gamma(3.141, 0, 0.282)
+    normA2_p = Gamma(1, 0, 1)
 
     phase1_e = parameter.Uniform(0, 2 * np.pi)(name + '_phase1_e')
     phase2_e = parameter.Uniform(0, 2 * np.pi)(name + '_phase2_e')
@@ -110,10 +110,10 @@ def dpdm_block(model=None,log10_ma=None,log10_eps=None,normA1_e=None,normA2_e=No
     if model == "U1B":
         delay = delay_U1B(log10_ma = log10_ma,
                         log10_eps = log10_eps,
-                        normA1_e = normA1_e,
-                        normA2_e = normA2_e,
-                        normA3_e = normA3_e,
-                        normA_p = normA_p,
+                        normA2_1_e = normA2_1_e,
+                        normA2_2_e = normA2_2_e,
+                        normA2_3_e = normA2_3_e,
+                        normA2_p = normA2_p,
                         phase1_e = phase1_e,
                         phase2_e = phase2_e,
                         phase3_e = phase3_e,
@@ -123,10 +123,10 @@ def dpdm_block(model=None,log10_ma=None,log10_eps=None,normA1_e=None,normA2_e=No
     if model == "U1BL":
         delay = delay_U1BL(log10_ma = log10_ma,
                         log10_eps = log10_eps,
-                        normA1_e = normA1_e,
-                        normA2_e = normA2_e,
-                        normA3_e = normA3_e,
-                        normA_p = normA_p,
+                        normA2_1_e = normA2_1_e,
+                        normA2_2_e = normA2_2_e,
+                        normA2_3_e = normA2_3_e,
+                        normA2_p = normA2_p,
                         phase1_e = phase1_e,
                         phase2_e = phase2_e,
                         phase3_e = phase3_e,
